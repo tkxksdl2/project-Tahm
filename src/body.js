b52be2420a9e6f26168a5ad8b771f73c47f6bd61
tkxksdl2 <tkxksdl2@gmail.com>
2022-03-02 15:21:22 +0900

    2022/03/02 statboard

--- src/body.js
+++ src/body.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./sidebar";
 
 const Body = () => {
+    const [state, setState] = useState({
+        hp:[1000, 1800, 3240],
+        mp: 30,
+        ad:[80, 144, 259.2],
+        ap:100,
+        def:60,
+        mr:60,
+        skil: [900, 1350, 30000]
+    });
+    const [tahmStar, setStar] = useState({
+        star:0
+    });
+    // useEffect(() => {
+
+    // }, []);
+
+
+
     const bodyStyle = {
         height:"100vh",
         display:"flex",
@@ -19,13 +37,41 @@ const Body = () => {
         marginLeft:"-125px",
         
     }
+
+    const statBoard = {
+        margin:"1rem",
+        padding:"0.4rem",
+        border:"1px solid",
+        height:"210px"
+    }
     return(
         <div style={bodyStyle}>
-            <div>
-                <li>
-                스탯창
-                </li>
-                HP:100
+            <div style ={statBoard}>
+                <div>
+                    Here's Tahm Kench's stats!
+                </div>
+                <div>
+                    HP : {state.hp[tahmStar.star]}
+                </div>
+                <div>
+                    MP : {state.mp}
+                </div>
+                <div>
+                    DEF : {state.def}
+                </div>
+                <div>
+                    MR : {state.mr}
+                </div>
+                <div>
+                    AD : {state.ad[tahmStar.star]}
+                </div>
+                <div>
+                    AP : {state.ap - 100}
+                </div>
+                <div>
+                    Skil Damage : {state.skil[tahmStar.star] * state.ap/100}
+                </div>
+
             </div>
 
             <div style={{textAlign:"center", order:1, flexGrow:1}}>
